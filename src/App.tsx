94ef2f26dabc7322629b5b12ec47d0546135486c
thegrimbee <gabriel.mario.antaputra@gmail.com>
2026-02-19 00:21:03 +0800

    Added object details and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-// src/App.tsx
-import { useState } from 'react'
-import { ObjectGraph } from './assets/ObjectGraph'
-import { mockObjects } from './assets/MockData'
+import { ObjectDatabase } from "./assets/ObjectDatabase"
 
+// src/App.tsx
 function App() {
-  const [selectedHash, setSelectedHash] = useState<string | undefined>()
 
   return (
-    <div className="w-screen h-screen bg-[#1e1e1e] flex flex-col">
-      <div className="flex-1 overflow-hidden relative">
-        <ObjectGraph 
-          objects={mockObjects} 
-          selectedHash={selectedHash}
-          onSelectObject={(hash) => {
-            console.log('Selected:', hash)
-            setSelectedHash(hash)
-          }}
-        />
-      </div>
-    </div>
+            <ObjectDatabase />
+
   )
 }
 
